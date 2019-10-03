0d53d2fd9df823aa3f22f53f7b9a900c884803be
eliasolson23 <55261810+eliasolson23@users.noreply.github.com>
2019-10-03 18:12:57 -0500

    Created Moving ramp support that moves with player input

---
+++ Vigilant-Waffle Unity Project/Assets/Scripts/MovingPlatformLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformLeft : MonoBehaviour
+{
+    private float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sup");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player presses up arrow,
+        // the square moves up.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -moveSpeed, 0);
+        }
+        //Debug.Log("UPDATE METHOD"); 
+    }
+}
