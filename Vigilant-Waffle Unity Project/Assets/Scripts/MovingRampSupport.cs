d85ff8a8c3c45948a5da8023269eb899fc01a972
eliasolson23 <55261810+eliasolson23@users.noreply.github.com>
2019-10-10 18:06:13 -0500

    Changed move system to use physics

---
+++ Vigilant-Waffle Unity Project/Assets/Scripts/MovingRampSupport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampSupport : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    private float verticalInput;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(0, verticalInput * moveSpeed);
+    }
+}
