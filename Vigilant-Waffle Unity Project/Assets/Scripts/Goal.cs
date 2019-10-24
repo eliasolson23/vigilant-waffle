c54bd793397908ab146318077b268fc890237649
eliasolson23 <55261810+eliasolson23@users.noreply.github.com>
2019-10-24 18:08:23 -0500

    Added Goal and Win Text

---
+++ Vigilant-Waffle Unity Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///Detects when the ball touches the goal, and then win the level.
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Success!");
+        }
+    }
+
+    
+}
