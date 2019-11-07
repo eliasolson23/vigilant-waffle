6a55181fb7dad7d333ea63f61c139e496d771d39
eliasolson23 <55261810+eliasolson23@users.noreply.github.com>
2019-11-07 17:56:24 -0600

    Sound

--- Vigilant-Waffle Unity Project/Assets/Scripts/Goal.cs
+++ Vigilant-Waffle Unity Project/Assets/Scripts/Goal.cs
@@ -6,11 +6,22 @@ using UnityEngine;
 /// </summary>
 public class Goal : MonoBehaviour
 {
+    public GameObject winText;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
       
         if (collision.tag == "Player")
         {
+            winText.SetActive(true);
+
+            audioSource.Play();
             Debug.Log("Success!");
         }
     }
